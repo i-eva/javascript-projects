c7f9c3f9b311f166eb09cba6fec2525262418d52
Ieva Swanson <ievaswanson@u.boisestate.edu>
2023-12-20 16:37:58 -0600

    Practice with javascript

--- how-to-write-code/Comments.js
+++ how-to-write-code/Comments.js
@@ -1,12 +1,21 @@
    // This demo shows off comments!
 
-   // console.log("This does not print.");
+   // I am practicing adding comments!
 
-   console.log("Hello, World!"); // Comments do not have to start at the beginning of a line.
+   console.log("'This does not print' is no longer true when I remove the comment syntax");
 
+   console.log("Hello, World! Unseen by y'all, I have added comments."); // Comments do not have to start at the beginning of a line.
+
+   console.log("For example, this line is follwed by a silly comment."); // A silly comment
+   
    /* Here is how
    to have
    multi-line
    comments. */
 
-   console.log("Comments make your code more readable by others.");
+   console.log("Comments make your code more readable by others, unless your comment itself is unreadable.");
+
+   /* Now I lay me down to sleep
+   in my thoughts and dreams to steep
+   my over-weary wrinkled brain
+   free of logic's rational train */ 
--- how-to-write-code/consolelogexamples02.js
+++ how-to-write-code/consolelogexamples02.js
@@ -1,3 +1,7 @@
 console.log("Some Programming Languages:");
 
 console.log("Python\nJavaScript\nJava\nC#\nSwift");
+
+console.log("\nA list of 1980's mass-market innovations:");
+
+console.log("\tAnswering machines\n\tVideo cassette recorders\n\tHome desktop computers");
