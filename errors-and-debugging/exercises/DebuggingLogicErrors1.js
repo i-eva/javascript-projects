ed33e00b573aebb0473d320d19e94436a114b700
Ieva Swanson <ievaswanson@u.boisestate.edu>
2024-01-10 21:00:53 -0600

    called out errors but did not fix code yet

--- errors-and-debugging/exercises/DebuggingLogicErrors1.js
+++ errors-and-debugging/exercises/DebuggingLogicErrors1.js
@@ -1,6 +1,17 @@
 // Run this sample code as-is and examine the output. 
+//WARNING: Insufficient fuel!
+//Crew & computer cleared.
+//10, 9, 8, 7, 6, 5, 4, 3, 2, 1...
+//Liftoff!
 // Should the shuttle have launched? 
+
+//No, there wasn't enough fuel
+
 // Did it?
+
+//yes, because after the program set launchReady to false, it reset it to true
+//Basically, it either needed to abort checking other readiness factors, or create some && statements
+
 // Do not worry about fixing the code yet, we will do that in the next series of exercises.
 
 let launchReady = false;
