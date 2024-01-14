a877a8b32de862857101a3794012ff0435a27118
Ieva Swanson <ievaswanson@u.boisestate.edu>
2024-01-14 11:33:52 -0600

    Chapter 7 practice with template literals

--- stringing-characters-together/code-snippets/mad-libs.js
+++ stringing-characters-together/code-snippets/mad-libs.js
@@ -1,7 +1,15 @@
-let pluralNoun = ;
-let name = ;
-let verb = ;
-let adjective = ;
-let color = ;
+let pluralNoun = "dogs";
+let name = "Edna";
+let verb = "skate";
+let adjective = "crisp";
+let color = "blue";
 
-console.log("JavaScript provides a "+ color +" collection of tools — including " + adjective + " syntax and " + pluralNoun + " — that allows "+ name +" to "+ verb +" with strings.")
+console.log("JavaScript provides a "+ color +" collection of tools — including " + adjective + " syntax and " + pluralNoun + " — that allows "+ name +" to "+ verb +" with strings.");
+
+let pluralNoun2 = "bongos";
+let name2 = "Beyonce";
+let verb2 = "cough";
+let adjective2 = "sharp";
+let color2 = "puce";
+
+console.log(`JavaScript provides a ${color2} collection of tools — including ${adjective2} syntax and ${pluralNoun2} — that allows ${name2} to ${verb2} with strings.`);
