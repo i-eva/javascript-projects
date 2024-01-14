da38ae0861b67488f37bd4f3c18c10c6000e2b6e
Ieva Swanson <ievaswanson@u.boisestate.edu>
2024-01-14 15:34:41 -0600

    more practice with template literals and string slicing

--- stringing-characters-together/exercises/part-two.js
+++ stringing-characters-together/exercises/part-two.js
@@ -4,18 +4,33 @@ let dna = " TCG-TAC-gaC-TAC-CGT-CAG-ACT-TAa-CcA-GTC-cAt-AGA-GCT    ";
 
 // First, print out the dna strand in it's current state.
 
+console.log(`There are errors in this string: 
+\t${dna}`);
+
 //1) Use the .trim() method to remove the leading and trailing whitespace, then print the result.
 
-console.log(/* Your code here. */);
+dnaFix = dna.trim();
+console.log(`First, let's trim away the leading and trailing white spaces:
+\t${dnaFix}`);
 
 //2) Change all of the letters in the dna string to UPPERCASE, then print the result.
 
-console.log();
+dnaFix = dnaFix.toUpperCase();
+console.log(`Next, let's capitalize all letters:
+\t${dnaFix}`);
 
-//3) Note that after applying the methods above, the original, flawed string is still stored in dna. To fix this, we need to reassign the changes to back to dna.
-//Apply these fixes to your code so that console.log(dna) prints the DNA strand in UPPERCASE with no whitespace.
+//3) Note that after applying the methods above, the original, flawed string is still stored in dna. 
+//To fix this, we need to reassign the changes to back to dna.
+//Apply these fixes to your code so that console.log(dna) prints 
+//the DNA strand in UPPERCASE with no whitespace.
 
-console.log(dna);
+console.log(`Let's compare the original string with the repaired string:`);
+
+console.log(`\tOriginal: ${dna}`);
+console.log(`\tRepaired: ${dnaFix}`);
+
+console.log(`\nLookin' good!\nOK time to update our original variable with the repairs.`)
+dna = dnaFix
 
 //Part Two Section Two
 
@@ -23,10 +38,28 @@ let dnaTwo = "TCG-TAC-GAC-TAC-CGT-CAG-ACT-TAA-CCA-GTC-CAT-AGA-GCT";
 
 //1) Replace the gene "GCT" with "AGG", and then print the altered strand.
 
-//2) Look for the gene "CAT" with ``indexOf()``. If found print, "CAT gene found", otherwise print, "CAT gene NOT found".
+console.log(`\nRight. Next steps. \nReplace GCT with AGG and check if CAT is found:`);
+console.log(`\t${dnaTwo.replace("GCT", "AGG")}`);
+
+//2) Look for the gene "CAT" with ``indexOf()``. 
+//If found print, "CAT gene found", 
+//otherwise print, "CAT gene NOT found".
+
+if (dna.indexOf('CAT')){
+    console.log(`\tCAT gene found.`);
+    } else {
+    console.log(`\tCAT gene NOT found.`);
+    }
 
 //3) Use .slice() to print out the fifth gene (set of 3 characters) from the DNA strand.
 
+console.log(`Use slice method to print the 5th gene: \n\t${dnaTwo.slice(16,19)}`);
+
 //4) Use a template literal to print, "The DNA strand is ___ characters long."
 
+console.log(`Count length:\n\tThe DNA strand is ${dnaTwo.length} characters long.`);
+
 //5) Just for fun, apply methods to ``dna`` and use another template literal to print, 'taco cat'.
+
+dnaTwo = dnaTwo.toLowerCase()
+console.log(`Now print '${dnaTwo.slice(4,7)}o ${dnaTwo.slice(40,43)}'`);
