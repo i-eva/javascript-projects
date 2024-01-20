f37e5912582426c37e58bf52742da9bfc67ab874
Ieva Swanson <ievaswanson@u.boisestate.edu>
2024-01-20 13:37:59 -0600

    for loop with an array

--- loops/chapter-examples/for-Loop-Practice-With-Arrays.js
+++ loops/chapter-examples/for-Loop-Practice-With-Arrays.js
@@ -1,3 +1,9 @@
 // create an array variable containing the names
 
+let myFamily = ["Sally","Jim","George","Steve","Sheryl"];
+
 // write a for loop that prints each name on a different line
+
+for (let i = 0; i < myFamily.length; i++) {
+    console.log(myFamily[i])
+}
