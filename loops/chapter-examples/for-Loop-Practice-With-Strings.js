364128fce25c22200dac5181d87cf315576a4f51
Ieva Swanson <ievaswanson@u.boisestate.edu>
2024-01-20 14:02:17 -0600

    checking answers to ch 9 questions

--- loops/chapter-examples/for-Loop-Practice-With-Strings.js
+++ loops/chapter-examples/for-Loop-Practice-With-Strings.js
@@ -7,3 +7,12 @@ let person = "iEvaSwanson"
 for (let i = 0; i < person.length; i++) {
     console.log(person[i]);
 }
+
+let phrase = "Chili Cook-off";
+
+//check answers to Ch 9 questions: 5, ClCkf
+
+for (let i = 0; i < phrase.length - 1; i = i + 3) {
+   console.log(phrase[i]);
+}
+
