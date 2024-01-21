316d3f25f025cdd64d11bcee7f30dcfccab8a944
Ieva Swanson <ievaswanson@u.boisestate.edu>
2024-01-21 14:58:17 -0600

    having trouble with readline-sync

--- loops/exercises/while-Loop-Exercises.js
+++ loops/exercises/while-Loop-Exercises.js
@@ -3,22 +3,22 @@ const input = require('readline-sync');
 //Define three variables for the LaunchCode shuttle - one for the starting fuel level, 
 //another for the number of astronauts aboard, and the third for the altitude the shuttle reaches.
 
-let fuelStart
-let astroNum
-let shutAlt
+//let fuelStart
+//let astroNum
+//let shutAlt
 
 /*Exercise #4: Construct while loops to do the following:
   a. Query the user for the starting fuel level. 
   //Validate that the user enters a positive, integer value 
   //greater than 5000 but less than 30000. */
 
-fuelStart = input.question("Enter the starting fuel level: ");
-fuelStart = Number(fuelStart);
+let fuelStart = input.question("Enter the starting fuel level: ");
+//fuelStart = Number(fuelStart);
 
-while (fuelStart <= 0 && fuelStart > 5000 && fuelStart < 30000) {
-   fuelStart = input.question('Invalid input. Please enter the starting fuel level:');
-   fuelStart = Number(fuelStart);
-}
+//while (fuelStart <= 0 && fuelStart > 5000 && fuelStart < 30000) {
+//   fuelStart = input.question('Invalid input. Please enter the starting fuel level:');
+//   fuelStart = Number(fuelStart);
+//}
 
 //b. Use a second loop to query the user for the number of astronauts 
 //(up to a maximum of 7). Validate the entry.
