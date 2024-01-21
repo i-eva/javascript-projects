7e71acd456e0a16987835d1b371d383e99f4b8a8
Ieva Swanson <ievaswanson@u.boisestate.edu>
2024-01-21 12:02:01 -0600

    completed exercise 1a-c  but not challenge 1d

--- loops/exercises/for-Loop-Exercises.js
+++ loops/exercises/for-Loop-Exercises.js
@@ -2,10 +2,48 @@
     a. Print the numbers 0 - 20, one number per line.
     b. Print only the ODD values from 3 - 29, one number per line.
     c. Print the EVEN numbers 12 to -14 in descending order, one number per line.
-    d. Challenge - Print the numbers 50 - 20 in descending order, but only if the numbers are multiples of 3. (Your code should work even if you replace 50 or 20 with other numbers). */
+    d. Challenge - Print the numbers 50 - 20 in descending order, 
+    //but only if the numbers are multiples of 3. 
+    //(Your code should work even if you replace 50 or 20 with other numbers). */
 
+console.log(`Exercise #1a result: 
+output of a for loop that prints the numbers 0 - 20, 
+one number per line.`);
 
+for (let i = 0; i < 21; i++) {
+  console.log(i)
+}
 
+console.log(`Exercise #1b result: 
+output of a for loop that prints only the ODD values from 3 - 29, 
+one number per line.`);
+
+for (let i = 3; i < 30; i = i+2) {
+  console.log(i)
+}
+
+console.log(`Exercise #1c result: 
+output of a for loop that prints the EVEN numbers 12 to -14 in descending order, 
+one number per line.`);
+
+for (let i = 12; i > -15; i = i-2) {
+  console.log(i)
+}
+
+console.log(`Exercise #1d result: output of a for loop that
+prints the numbers 50 - 20 in descending order, 
+but only if the numbers are multiples of 3. 
+My code should work even if I replace 50 or 20 with other numbers.`);
+
+for (let i = 50; i%3 === 0 ; i = i--) {
+  console.log(i)
+  if (i < 20) {
+    break;
+  }
+}
+
+console.log(`...
+My code attempts don't work... YET`);
 
 /*Exercise #2: 
 Initialize two variables to hold the string “LaunchCode” and the array [1, 5, ‘LC101’, ‘blue’, 42].
