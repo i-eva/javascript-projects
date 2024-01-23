ec312944acb5efe06f853097c62770491f6a4920
Ieva Swanson <ievaswanson@u.boisestate.edu>
2024-01-23 16:43:20 -0600

    Practice from Chapter 10 reading

--- functions/try-it/sayHello.js
+++ functions/try-it/sayHello.js
@@ -1,3 +1,10 @@
+// Question: What is printed when this program  (lines 2-4) runs?
 function sayHello() {
    console.log("Hello, World!");
 }
+// Answer: my guess is nothing, because the function is never called
+// Nothing appered, so we'll try again and add a call line:
+
+sayHello();
+
+// This time, console.log was able to do it's thing
