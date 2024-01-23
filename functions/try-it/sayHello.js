fc9e816e1440e31d5c351a960d19ac80378d2344
Ieva Swanson <ievaswanson@u.boisestate.edu>
2024-01-23 17:06:17 -0600

    More practice from Ch 10

--- functions/try-it/sayHello.js
+++ functions/try-it/sayHello.js
@@ -8,3 +8,20 @@ function sayHello() {
 sayHello();
 
 // This time, console.log was able to do it's thing
+
+// Question: What does the following code output?
+function plusTwo(num) {
+   return num + 2;
+}
+
+let a = 2;
+
+for (let i=0; i < 4; i++) {
+   a = plusTwo(a);
+}
+
+console.log(a);
+
+//Answer: My guess is a in this for loop reaches 8
+//Alas I began with the value 2 instead of 4. The correct answer is 10.
+// a should become 4 after round 0, then 6, then 8 on round 2, not three
