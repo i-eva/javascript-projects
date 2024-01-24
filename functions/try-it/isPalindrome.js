eb687d7aecd4eb3b07dff43f3e617c519beecf5a
Ieva Swanson <ievaswanson@u.boisestate.edu>
2024-01-23 18:38:50 -0600

    The palindrome of Bolton would be notlob!

--- functions/try-it/isPalindrome.js
+++ functions/try-it/isPalindrome.js
@@ -1,7 +1,12 @@
+// Test the function with a few cases, such as str = "notlob" , "racecar"
+let str = "bolton";
+
 function reverse(str) {
-   return str.split('').reverse().join('');
+   return str.split("").reverse().join("");
 }
+console.log(reverse(str));
 
 function isPalindrome(str) {
    return reverse(str) === str;
 }
+console.log(isPalindrome(str));
