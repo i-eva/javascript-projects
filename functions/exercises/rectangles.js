f9e073f66ed9c1bd1b55c7e0c6c6159e72ae042b
Ieva Swanson <ievaswanson@u.boisestate.edu>
2024-01-24 20:05:17 -0600

    created function makeRectangle(5, 3) on my own

--- functions/exercises/rectangles.js
+++ functions/exercises/rectangles.js
@@ -25,7 +25,7 @@ ${makeLine(5)}
 
 function makeSquare(size) {
     let row = makeLine(size);
-    let square = row
+    let square = row;
     for (let i = 0; i < size-1; i++ ) {
         square += "\n" + row;
     }
@@ -35,7 +35,7 @@ function makeSquare(size) {
 
 console.log(`The function makeSquare(5) produces
 ${makeSquare(5)}
-`)
+`);
 
 //  Console output should be:
     // #####
@@ -43,3 +43,26 @@ ${makeSquare(5)}
     // #####
     // #####
     // ##### 
+// After getting help with program logic I was able to make this work
+
+//Write a function makeRectangle(width, height) that returns a rectangle 
+//with the given width (5) and height (3). Use your makeLine function to do this.
+
+function makeRectangle(width,height) {
+    let size = width
+    width = makeLine(size);
+    let rectangle = width;
+    for (let i = 0; i < height-1; i++ ) {
+        rectangle += "\n" + width;
+    }
+    return rectangle;
+}
+
+console.log(`The function makeRectangle(5,3) produces
+${makeRectangle(5,3)}
+`);
+
+// expected output:
+// #####
+// #####
+// #####
