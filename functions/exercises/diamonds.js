eb9104457b9f204c228be2aaff805c9c707dbb06
Ieva Swanson <ievaswanson@u.boisestate.edu>
2024-01-24 22:37:23 -0600

    completed diamond pattern

--- functions/exercises/diamonds.js
+++ functions/exercises/diamonds.js
@@ -29,16 +29,18 @@ function makeSpaceLine(numSpaces,numChars) {
 }
 
 function makeIsoscelesTriangleUp(height) {
-    let layer = makeSpaceLine(height-1,1)
-    for (let i = 1; i < height; i++ ) {
-        layer += "\n" + makeSpaceLine((height-(i+1)),((2*i)+1));
+    let up = height
+    let layer = makeSpaceLine(up-1,1)
+    for (let i = 1; i < up; i++ ) {
+        layer += "\n" + makeSpaceLine((up-(i+1)),((2*i)+1));
     }
     return layer;
 }
 function makeIsoscelesTriangleDown(height) {
-    let layer = makeSpaceLine(0,((2*height)+1))
-    for (let i = height-1; i > 0; i-- ) {
-        layer += "\n" + makeSpaceLine(i,((2*i)+1));
+    let down = height
+    let layer = "\n" + makeSpaceLine(0,(2*down)-1)
+    for (let i = down - 1; i > 0; i-- ) {
+    layer += "\n" + makeSpaceLine(down-i,((2*i)-1));
     }
     return layer;
 }
