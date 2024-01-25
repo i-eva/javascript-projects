3bde646dd208836ccc75821cf6b8c45b75aeac24
Ieva Swanson <ievaswanson@u.boisestate.edu>
2024-01-24 19:32:15 -0600

    managed to get makeSquare function to work as directed

--- functions/exercises/rectangles.js
+++ functions/exercises/rectangles.js
@@ -8,15 +8,38 @@ function makeLine(size) {
     return line;
 }
 
-console.log(makeLine(5));
+console.log(`The function makeLine(5) produces
+${makeLine(5)}
+`);
 
-//console output should be:  #####
+//  console output should be:  #####
+//  After checking the answer in the textbook I got the desired result.
 
-// My attempt gave an output "undefined."
-// Notes on where i went wrong: 
-// My line #3 was correct.
-// My line #4 was incorrect.
-// My line #5 was partially correct.
-// My line #6 was way off.
-// My curly brackets were correct but I forgot the return line.
-// I attempted to set up an array instead of using a variable. 
+// Write a function called makeSquare(size) that returns a size by size string of hashes. 
+// The function should NOT print each row of the square. 
+// Instead, it must return a single string that contains the entire shape.
+// Tips
+//  Call your makeLine function to create each row of the square.
+//  The newline character, \n, will be helpful to you.
+//  Do NOT include a newline character at the end of your string.
+
+function makeSquare(size) {
+    let row = makeLine(size);
+    let square = row
+    for (let i = 0; i < size-1; i++ ) {
+        square += "\n" + row;
+    }
+    return square;
+}
+
+
+console.log(`The function makeSquare(5) produces
+${makeSquare(5)}
+`)
+
+//  Console output should be:
+    // #####
+    // #####
+    // #####
+    // #####
+    // ##### 
