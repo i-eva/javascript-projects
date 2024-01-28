67b2169785d04f4662bd636eee90c78e7d902288
Ieva Swanson <ievaswanson@u.boisestate.edu>
2024-01-28 12:13:33 -0600

    able to code validator for input begins with a; unable to code validator for input begins with a vowel (yet)

--- more-on-functions/chapter-examples/receieve-function-arguments/getValidInput.js
+++ more-on-functions/chapter-examples/receieve-function-arguments/getValidInput.js
@@ -15,7 +15,22 @@ function getValidInput(prompt, isValid) {
 // TODO 1: write a validator 
 // that ensures input starts with "a"
 
+// A boolean function for validating input
+let sliceFirstLetter = function(a) {
+  return a.slice(0,1) === "a";
+};
+
+console.log(getValidInput('Enter a word beginning with lowercase "a": ', sliceFirstLetter));
+
+
 // TODO 2: write a validator 
 // that ensures input is a vowel
+
+// I thought I could do this one now... 
+// ...after trying for about an hour I learned
+// I can't do this one yet. 
+// I have to move on to get through the chapter, the quiz, and
+// the exercises.
+
 
 // Be sure to test your code!
