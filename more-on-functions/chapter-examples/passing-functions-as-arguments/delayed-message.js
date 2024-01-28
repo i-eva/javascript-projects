82bb0cd3e8acd7a1540d44d5fbd2635639c37d1d
Ieva Swanson <ievaswanson@u.boisestate.edu>
2024-01-28 08:39:47 -0600

    practice named and anon coding with setTimeout func

--- more-on-functions/chapter-examples/passing-functions-as-arguments/delayed-message.js
+++ more-on-functions/chapter-examples/passing-functions-as-arguments/delayed-message.js
@@ -1,5 +1,17 @@
+/* Suppose we want to log a message with a 5 second delay. 
+Since five seconds is 5000 milliseconds 
+(1 second = 1000 milliseconds), we can do so like this:
+*/
+
 function printMessage() {
-    console.log("The future is now!");
+    console.log("The future is less now than before!");
 }
 
-setTimeout(printMessage, 5000);
+setTimeout(printMessage, 9000);
+
+// As an anon func:
+
+setTimeout(function () {
+    console.log("The future is now!");
+}, 5000);
+
