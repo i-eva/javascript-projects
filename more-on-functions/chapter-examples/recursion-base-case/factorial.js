1aa5b6b30d1f9c6620dee43536190d7e4abd5cce
Ieva Swanson <ievaswanson@u.boisestate.edu>
2024-01-28 16:41:36 -0600

    recursive function calling itself - practice

--- more-on-functions/chapter-examples/recursion-base-case/factorial.js
+++ more-on-functions/chapter-examples/recursion-base-case/factorial.js
@@ -1,13 +1,25 @@
 //The following concept check assumes that only positive integers are passed to the function.
 
 function factorial(integer){
-  if (/*enter base case test here*/){
+  if (integer === 1){
     return integer;
   } else {
     return integer*(factorial(integer-1));
   }
 }
 
-console.log(factorial(4));
+console.log(factorial(2));
 
-//Skill boost! Add validation to return an error message if the function is passed a string, negative number or a decimal value.
+//Skill boost! Add validation to return an error message 
+//if the function is passed a string, negative number or a decimal value.
+
+function decreasingSum(integer) {
+  if (integer === 1){
+  return integer;
+  } else {
+  //call decreasingSum function again return 
+    return integer + (decreasingSum(integer-1));
+  }
+}
+
+console.log(decreasingSum(5));
