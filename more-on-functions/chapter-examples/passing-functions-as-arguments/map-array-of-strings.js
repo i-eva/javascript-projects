5066e9e0635d1811904ace502805aa1d7483fd1d
Ieva Swanson <ievaswanson@u.boisestate.edu>
2024-01-28 10:41:27 -0600

    used map function to pass another function as an argument

--- more-on-functions/chapter-examples/passing-functions-as-arguments/map-array-of-strings.js
+++ more-on-functions/chapter-examples/passing-functions-as-arguments/map-array-of-strings.js
@@ -1,7 +1,34 @@
-let names = ["Chris", "Jim", "Sally", "Blake", "Paul", "John", "Courtney", "Carly"];
+// Example #1, named mapping function
+
+let numsOne = [3.14, 42, 4811];
+
+let timesTwo = function (n) {
+    return n*2;
+};
+
+let doubledOne = numsOne.map(timesTwo);
+
+console.log(numsOne);
+console.log(doubledOne);
+
+
+// Example #2, anon mapping function
+
+let nums = [3.14, 42, 4811];
+
+let doubled = nums.map(function (n) {
+    return n*2;
+});
+
+console.log(doubled);
 
 // TODO: Write a mapping function
+
+let names = ["Chris", "Jim", "Sally", "Blake", "Paul", "John", "Courtney", "Carly"];
+let sliceInitial = function (name) {
+    return name.slice(0,1);
+};
 // and pass it to .map()
-let firstInitials = names.map();
+let firstInitials = names.map(sliceInitial);
 
 console.log(firstInitials);
