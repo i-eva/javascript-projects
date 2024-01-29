4d4e995c4c8b06f4e574968279c765cfeac49f04
Ieva Swanson <ievaswanson@u.boisestate.edu>
2024-01-28 20:30:30 -0600

    with help from textbook solutions... exercise completed

--- more-on-functions/exercises/practice-your-skills.js
+++ more-on-functions/exercises/practice-your-skills.js
@@ -6,33 +6,59 @@ b) If passed a string, return the string “ARRR!”
 c) Be sure to test your function before moving on the next part. 
 */
 
-const input = require('readline-sync');
+// const input = require('readline-sync');
 
-function getValidInput(prompt, isValid) {
+// function getValidInput(prompt, isValid) {
 
-    // Prompt the user, using the prompt string that was passed
-    let userInput = input.question(prompt);
+//     // Prompt the user, using the prompt string that was passed
+//     let userInput = input.question(prompt);
 
-    // Call the boolean function isValid to check the input
-    while (!isValid(userInput)) {
-    console.log("ARRR!");
-    userInput = input.question(prompt);
+//     // Call the boolean function isValid to check the input
+//     while (!isValid(userInput)) {
+//     console.log("ARRR!");
+//     userInput = input.question(prompt);
+//     }
+
+//     return userInput;
+// }
+
+// // A boolean function for validating input
+// let isNumber = function(n) {
+//     return Number(n);
+// };
+
+// // A math function for tripling valid input
+// let tripleValue = function(n) {
+//     return 3*n;
+// };
+
+// console.log(getValidInput('Enter an even number: ', tripleValue));
+
+
+
+// Here is the solution from the textbook:
+
+let practice = function(myArg) {
+    if (typeof myArg === "number") {
+       return myArg * 3;
     }
+ };
+ console.log(practice(9));
 
-    return userInput;
-}
 
-// A boolean function for validating input
-let isNumber = function(n) {
-    return Number(n);
-};
+ let practiceTwo = function(myArg) {
+    if (typeof myArg === "number") {
+       return myArg * 3;
+    } else if (typeof myArg === "string") {
+       return "ARRR!";
+    } else {
+       return myArg;
+    }
+ 
+ };
 
-// A math function for tripling valid input
-let tripleValue = function(n) {
-    return 3*n;
-};
-
-console.log(getValidInput('Enter an even number: ', tripleValue));
+console.log(practiceTwo("betsy"));
+console.log(practiceTwo(9));
 
 /* Add to your code! Use your fuction and the map method to change an array  as follows:
 a) Triple any the numbers.
@@ -40,4 +66,12 @@ b) Replace any strings with “ARRR!”
 c) Print the new array to confirm your work.
 */
 
+
 let arr = ['Elocution', 21, 'Clean teeth', 100];
+
+let mapped = arr.map(practiceTwo);
+
+console.log(arr);
+console.log(mapped);
+
+
