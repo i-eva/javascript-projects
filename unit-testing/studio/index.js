9ddf63f0834c1bd4371b89912e5e47992d21a45f
Ieva Swanson <ievaswanson@u.boisestate.edu>
2024-02-08 20:43:16 -0600

    studio

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,12 @@
 
 let launchcode = {
-  
-}
+  organization: "nonprofit",
+  executiveDirector: "Jeff",
+  percentageCoolEmployees: 100,
+  programsOffered: ["Web Development", "Data Analysis", "Liftoff"]
+};
+
+console.log(launchcode.executiveDirector);
 
 module.exports = launchcode;
 
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -1,8 +1,28 @@
 // launchcode.test.js code:
 const launchcode = require('../index.js');
 
-describe("Testing launchcode", function(){
+describe("Testing launchcode", function() {
 
-  // Write your unit tests here!
+  test("Testing property 1 - organization : nonprofit", function() {
+    let result = (launchcode.organization);
+    expect(result).toBe("nonprofit");
+  });
+
+  test("Testing property 2 - executiveDirector : Jeff", function() {
+    let result = (launchcode.executiveDirector);
+    expect(result).toBe("Jeff");
+    });
   
-});
+  test("Testing property 3 - percentageCoolEmployees: 100", function() {
+    let result = (launchcode.percentageCoolEmployees);
+    expect(result).toBe(100);
+    });
+
+  test("Testing property 4 - programsOffered : [array]"), {
+    let result = (launchcode.programsOffered);
+    expect(result).toBe("[ 'Web Development', 'Data Analysis', 'Liftoff' ]");    
+  }
+
+  });
+
+  
