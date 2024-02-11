4b05213f75719c080bfa688f2a2e655ab33802ec
Ieva Swanson <ievaswanson@u.boisestate.edu>
2024-02-11 09:00:51 -0600

    learned that syntax errors in test files will throw an error when attempting to commit and push to github

--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -1,28 +1,4 @@
 // launchcode.test.js code:
 const launchcode = require('../index.js');
 
-describe("Testing launchcode", function() {
-
-  test("Testing property 1 - organization : nonprofit", function() {
-    let result = (launchcode.organization);
-    expect(result).toBe("nonprofit");
-  });
-
-  test("Testing property 2 - executiveDirector : Jeff", function() {
-    let result = (launchcode.executiveDirector);
-    expect(result).toBe("Jeff");
-    });
-  
-  test("Testing property 3 - percentageCoolEmployees: 100", function() {
-    let result = (launchcode.percentageCoolEmployees);
-    expect(result).toBe(100);
-    });
-
-  test("Testing property 4 - programsOffered : [array]"), {
-    let result = (launchcode.programsOffered);
-    expect(result).toBe("[ 'Web Development', 'Data Analysis', 'Liftoff' ]");    
-  }
-
-  });
-
   
