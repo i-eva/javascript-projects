fb22e62998294102856125455cf9cabdfbcdf94f
Ieva Swanson <ievaswanson@u.boisestate.edu>
2024-02-12 16:23:42 -0600

    ch 10 exercise completed

--- exceptions/chapter-examples/throw-default-error.js
+++ exceptions/chapter-examples/throw-default-error.js
@@ -1,1 +1,10 @@
-throw Error('You cannot divide by zero!');
+function divide(numerator, denominator) {
+    if (denominator === 0) {
+       throw Error('You cannot divide by zero!');
+    }
+    return numerator/denominator;
+ };
+
+ console.log(divide(100, 50));
+ console.log(divide(100, 20));
+ console.log(divide(100, 0));
