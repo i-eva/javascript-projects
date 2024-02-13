963274025b370f69882001cd69807ae3ad0f8dc9
Ieva Swanson <ievaswanson@u.boisestate.edu>
2024-02-12 19:54:11 -0600

    Ch 18 practice constructing classes

--- classes/chapter-examples/ClassExamples02.js
+++ classes/chapter-examples/ClassExamples02.js
@@ -1,6 +1,12 @@
-// Use terminal commands to see what happens when we call Astronaut but do not pass in 3 arguments.
+// Use terminal commands to see what happens when we call Astronaut 
+// but do not pass in 3 arguments.
+
+//The code returns "undefined" for the missing argument
 
 // Next, set default values for 1 or more of the parameters in constructor.
+
+// The defaults we set (this.age = 90, this.mass=100, solved the "undefined"
+// output for this.mass, but overrode the input of 120 for age.)
 
 class Astronaut {
    constructor(name, age, mass){
@@ -15,3 +21,11 @@ let tortoise = new Astronaut('Speedy', 120);
 console.log(tortoise.name, tortoise.age, tortoise.mass);
 
 // What happens if we call Astronaut and pass in MORE than 3 arguments? TRY IT!
+
+// First I restored the defaults this.age to age and this.mass to mass. 
+
+let tortoise2 = new Astronaut('Flash', 190, 4651, "Plumber");
+
+console.log(tortoise2.name, tortoise2.age, tortoise2.mass, tortoise2.role);
+
+// The extra value, "Plumber," did not print. Instead the output was "undefined"
