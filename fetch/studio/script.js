7e3cdaad8ada2882ab26a1c90b656437b7f8d066
Ieva Swanson <ievaswanson@u.boisestate.edu>
2024-03-04 20:43:17 -0600

    studio

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,1 +1,12 @@
-//TODO: Add Your Code Below
+window.addEventListener("load", function() {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+        response.json().then(function(json) {
+            console.log(json);
+            const firstName = document.getElementById("firstName");
+            const lastName = document.getElementById("lastName");
+            const hours = document.getElementById("hoursInSpace");
+
+
+        });
+    });
+});
